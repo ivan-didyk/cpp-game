dce756c7bbcb8c298930a74fb38215b4379a0224
Ivan Didyk <didyk.ivan.90@mail.ru>
2021-01-15 10:48:11 +0300

    📐 В меню появилась нерабочая кнопка

--- scenes/menu.hpp
+++ scenes/menu.hpp
@@ -2,6 +2,7 @@
 #include "lib/components/sprite.hpp"
 #include "lib/components/windowRender.hpp"
 #include "lib/components/text.hpp"
+#include "lib/components/dotRender.hpp"
 #include "components/center.hpp"
 #include <ncurses.h>
 
@@ -9,13 +10,24 @@ class Menu : public Scene {
   Transform* illustrationPos;
 
   void create() {
-    auto illustration = root()->addChild<Object>("bg");
-    illustration->addComponent<Sprite>("./sprites/mainmenu/main.xml");
-    illustration->addComponent<Center>();
 
-    /*auto btn = root()->addChild<Object>("btn");
-    btn->getTransform()->setAbsolutePosition({5, 5});
-    btn->addComponent<WindowRender>(10, 2);*/
+    auto container = root()->addChild<Object>("bg");
+    //container->addComponent<DotRender>();
+    container->addComponent<Center>();
+    container->getTransform()->setAbsolutePosition({ 15, 15 });
+
+    auto btn = container->addChild<Object>("btn");
+    btn->getTransform()->setRelativePosition({-10, 10});
+    btn->addComponent<WindowRender>(20, 2, COLOR_GREEN);
+    //btn->addComponent<DotRender>();
+
+    auto btnText = btn->addChild<Object>("btn-text");
+    btnText->addComponent<Text>("Примеры");
+    btnText->getTransform()->setRelativePosition({ 7, 1 });
+
+    auto illustration = container->addChild<Object>("illo");
+    //illustration->addComponent<DotRender>();
+    illustration->addComponent<Sprite>("./sprites/mainmenu/main.xml");
   }
 
   void update() {
