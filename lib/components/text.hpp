3509264e930ffa291ae9bf0f3b45f6a44933879b
Ivan Didyk <didyk.ivan.90@mail.ru>
2021-01-18 13:24:17 +0300

    🎫 Текст может быть цветным

--- lib/components/text.hpp
+++ lib/components/text.hpp
@@ -7,13 +7,17 @@
  */
 class Text : public Render {
   string txt;
+  int color = COLOR_WHITE;
 
   void draw() {
+    attron(COLOR_PAIR(color));
     mvprintw(getTransform()->y(), getTransform()->x(), "%s", txt.c_str());
+    attroff(COLOR_PAIR(color));
   }
  public:
   Text() { txt="Text..."; }
   Text(string t) { txt=t; }
+  Text(string t, int clr) { txt = t; color = clr; }
   void text(string newText) { txt=newText; }
   string text() { return txt; }
 };
