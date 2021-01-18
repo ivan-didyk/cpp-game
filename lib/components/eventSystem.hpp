ab262dd942674611ba533e833d1f207e40fdd107
Ivan Didyk <didyk.ivan.90@mail.ru>
2021-01-18 13:23:56 +0300

    🐛 Поправки в EventSystem

--- lib/components/eventSystem.hpp
+++ lib/components/eventSystem.hpp
@@ -1,26 +1,32 @@
 #pragma once
-#include "object.h"
+#include "lib/object.h"
 #include <functional>
 #include <map>
 using namespace std;
 
+struct EventData {
+
+};
+
+typedef void (*listener_t)(const EventData*);
+
 class EventSystem : public Component {
   // Event listeners - e.g. `Player.attacked()`, `Tree.spawned()`
-  map<string, vector<function<void(const EventData*)>>> listeners;
+  map<string, vector<listener_t>> listeners;
  public:
   // Emit an event
   void emit(string eventName, const EventData* data) {
-    for(const auto &i : listeners[eventName]) i(data);
+    for(const auto i : listeners[eventName]) (*i)(data);
   }
 
   // Add an listener
-  void listen(string eventName, function<void(const EventData*)> &listener) {
+  void listen(string eventName, listener_t listener) {
     listeners[eventName].push_back(listener);
   }
 
   // Remove listener
   // Returns false if the listener was not found
-  bool unlisten(string eventName, function<void(const EventData*)> &listener) {
+  bool unlisten(string eventName, listener_t listener) {
     auto &coll = listeners[eventName];
     auto ptr = find(coll.begin(), coll.end(), listener);
     if(ptr == coll.end()) return false;
