074fe520083fb6931622f355f44aee7af94343d4
Ivan Didyk <didyk.ivan.90@mail.ru>
2021-01-18 13:31:48 +0300

    📦 Кнопка переехала в отдельный объект

---
+++ object/button.hpp
@@ -0,0 +1,38 @@
+#include "lib/object.h"
+#include "lib/components/text.hpp"
+#include "lib/components/windowRender.hpp"
+#include "lib/utf8.hpp"
+#include "lib/components/eventSystem.hpp"
+#include <string>
+
+class Button : public Object {
+  string t;
+  unsigned width;
+  EventSystem * s;
+  Text* tt;
+
+  void create() {
+    width = utf8size(t.c_str())+3;
+    addComponent<WindowRender>(width, 2, COLOR_GREEN);
+    s = addComponent<EventSystem>();
+    auto btnText = addChild<Object>("btn-text");
+    tt = btnText->addComponent<Text>(t, COLOR_GREEN);
+    btnText->getTransform()->setRelativePosition({ 2, 1 });
+  }
+
+  void update() {
+    Vector2 pos = Game::getMousePosition();
+    if(Game::getMouseState() | BUTTON1_CLICKED) {
+      if(pos.x >= getTransform()->getAbsolutePosition().x && 
+         pos.x <= getTransform()->getAbsolutePosition().x + width &&
+         pos.y >= getTransform()->getAbsolutePosition().y &&
+         pos.y <= getTransform()->getAbsolutePosition().y + 2) {
+           s->emit("click", nullptr);
+         }
+    }
+  }
+public:
+  Button(string name, string txt) :Object(name) {
+    t = txt;
+  }
+};
--- scenes/menu.hpp
+++ scenes/menu.hpp
@@ -4,7 +4,14 @@
 #include "lib/components/text.hpp"
 #include "lib/components/dotRender.hpp"
 #include "components/center.hpp"
+#include "components/exit-scene.hpp"
+#include "object/button.hpp"
 #include <ncurses.h>
+
+void exitScene(const EventData* eventData) {
+  //printw("exitScene");
+  Game::openScene("key-demo");
+}
 
 class Menu : public Scene {
   Transform* illustrationPos;
@@ -16,23 +23,12 @@ class Menu : public Scene {
     container->addComponent<Center>();
     container->getTransform()->setAbsolutePosition({ 15, 15 });
 
-    auto btn = container->addChild<Object>("btn");
-    btn->getTransform()->setRelativePosition({-10, 10});
-    btn->addComponent<WindowRender>(20, 2, COLOR_GREEN);
-    //btn->addComponent<DotRender>();
-
-    auto btnText = btn->addChild<Object>("btn-text");
-    btnText->addComponent<Text>("Примеры");
-    btnText->getTransform()->setRelativePosition({ 7, 1 });
+    auto btn = container->addChild<Button>("btn", "Посмотреть примеры");
+    btn->getTransform()->setRelativePosition({ 10, 10 });
+    btn->getComponent<EventSystem>()->listen("click", exitScene);
 
     auto illustration = container->addChild<Object>("illo");
     //illustration->addComponent<DotRender>();
     illustration->addComponent<Sprite>("./sprites/mainmenu/main.xml");
-  }
-
-  void update() {
-    /*unsigned x, y;
-    getmaxyx(stdscr, y, x);
-    illustrationPos->xy(x/2, y/2 - 5);*/
   }
 };
